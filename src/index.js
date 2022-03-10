dd15b3d26f8349d582e54cae7691bedbb38aa8af
Lavadragon15396 <taddufort@gmail.com>
2022-03-10 07:15:44 +0000

    Handlers and a test command

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//! for formatting 
+const { Client, Intents } = require('discord.js');
+const fs = require("fs");
+const client = new Client({ intents: 32767 });
+const { Collection } = require('discord.js')
+require('dotenv').config();
+
+client.commands = new Collection();
+
+const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./src/commands");
+
+(async() => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, './src/events')
+    client.handleCommands(commandFolders, './src/commands')
+    client.login(process.env.token);
+})();
