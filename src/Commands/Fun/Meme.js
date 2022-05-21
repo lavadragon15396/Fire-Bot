146628580e7390df7cd41bf97557d60e41ced737
Lavadragon15396 <taddufort@gmail.com>
2022-05-21 15:12:32 +0100

    some fun commands

---
+++ src/Commands/Fun/Meme.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require(`node-fetch`);
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('meme')
+        .setDescription('Replies With A Random Meme!'),
+    async execute(interaction) {
+        start().then(memeEmbed => {
+            interaction.reply({embeds: [memeEmbed]})
+        });
+    },
+};
+
+async function start() {
+    return fetch(`https://meme-api.herokuapp.com/gimme`)
+        .then(res => res.json())
+        .then(json => {
+            const memeEmbed = new MessageEmbed()
+                .setTitle(json.title)
+                .setURL(json.postLink)
+                .setImage(json.url)
+                .setColor('FF8B00')
+                .setFooter({
+                    text: `r/${json.subreddit} | OP : ${json.author} | ${json.ups} upvotes`,
+                })
+                .setTimestamp();
+
+
+            if (json.nsfw) return start().then(console.log(`The meme ${json.postLink} was blocked because it is NSFW`));
+            return memeEmbed;
+        })
+}
