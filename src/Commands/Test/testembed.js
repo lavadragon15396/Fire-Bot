07378c03c6d4bbf54b419077aa4b130429016fdf
Lavadragon15396 <taddufort@gmail.com>
2022-05-21 14:43:59 +0100

    minor formatting tweaks

--- src/Commands/Test/testembed.js
+++ src/Commands/Test/testembed.js
@@ -1,9 +1,5 @@
-const {
-    SlashCommandBuilder
-} = require('@discordjs/builders');
-const {
-    MessageEmbed
-} = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 
 module.exports = {
