0441d6a71b811abb68e9a2203f03ac2d60830f02
Lavadragon15396 <taddufort@gmail.com>
2022-05-21 12:29:38 +0100

    delete old code

--- src/events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = { 
-    name: 'ready',
-    once: true,
-    async execute(client) {
-        console.log(`\n[2mStarted Bot With Token[0m: [3m[1m${process.env.token}[0m\n[2mLogged In As[0m: [3m[1m${client.user.tag} [0m \n`);
-    },
-};
