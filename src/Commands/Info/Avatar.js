7912200bf4e13d3a13a5e6d1276251dd13ef983b
Lavadragon15396 <taddufort@gmail.com>
2022-05-21 18:55:50 +0100

    avatar command

---
+++ src/Commands/Info/Avatar.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Replies With The Specified User\'s Avatar')
+        .addUserOption(option => option.setName('target').setDescription('The User To See The Avatar Of')),
+    async execute(interaction) {
+        const member = interaction.options.getMember('target');
+        const noMemberMentioned = new MessageEmbed()
+        .setTitle(`Please Mention A Member To View The Avatar Of`)
+        .setColor(`FF8B00`)
+        .setTimestamp()
+        const avatarEmbed = new MessageEmbed()
+        .setTitle (`${member.displayName}'s avatar`)
+        .setImage (member.displayAvatarURL())
+        .setColor (`FF8B00`)
+        .setTimestamp()
+        if (!member) {
+            await interaction.reply({embeds: [noMemberMentioned]});
+        } else {
+            await interaction.reply({embeds: [avatarEmbed]});
+        }
+    },
+};
