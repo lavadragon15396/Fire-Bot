435f2a88ab3723e06b3b60f4e01b874ccc8ff4dd
Lavadragon15396 <taddufort@gmail.com>
2022-05-28 17:17:51 +0100

    basic censoring system

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,11 +1,27 @@
-module.exports = { 
+module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
-        if (message.author.id === '651472179057262630') {
-            message.react('🔥');
-        };
-        if (message.author.id === '439870891543756831') {
-            message.react('🔥');
-        };
+        const badWords = ['nigger', 'nigga', 'faggot', 'pakki', 'paki', 'pakky', 'paky', 'negro', 'abbie', 'hebe',  ];
+
+        if (badWords.some(v => message.content.includes(v))) {
+            message.delete()
+        }
     },
 };
+
+// auto reactions
+//if (message.author.id === '651472179057262630') {
+//    message.react('🔥');
+//};
+//if (message.author.id === '439870891543756831') {
+//    message.react('🔥');
+//};
+//if (message.author.id === '782313476357881877') {
+//    message.react('🐦');
+//};
+
+// if (badWords.some(v => message.content.includes(v))) {
+//     console.log(`Match using "${message.content}"`);
+// } else {
+//     console.log(`No match using "${message.content}"`);
+// }
