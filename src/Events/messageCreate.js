596c58404713a45b93169dd130e3b9fcd26400c2
Lavadragon15396 <taddufort@gmail.com>
2022-05-29 21:17:38 +0100

    basic censoring

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,10 +1,20 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+
 module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
-        const badWords = ['nigger', 'nigga', 'faggot', 'pakki', 'paki', 'pakky', 'paky', 'negro', 'abbie', 'hebe',  ];
+        const badWords = ['nigger', 'nigga', 'niggo', 'neega', 'neegro', 'faggot', 'pakki', 'paki', 'negro', 'retard', 'retarded', 'test'];
+        const dontSlur = new MessageEmbed()
+            .setTitle(`Don't Slur ${message.author.username}`)
+            .setColor('F85056')
+            .setTimestamp()    
 
         if (badWords.some(v => message.content.includes(v))) {
             message.delete()
+            message.channel.send({ embeds: [dontSlur] })
+            message.channel.send({ content: `${message.author}` })
         }
     },
 };
