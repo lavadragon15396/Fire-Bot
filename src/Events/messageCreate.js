2804e7044f4949052b3c51038fb67bb4099f6a5b
Lavadragon15396 <taddufort@gmail.com>
2022-07-05 19:44:33 +0100

    more slurs added

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -5,33 +5,60 @@ const {
 module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
-        const badWords = ['nigger', 'nigga', 'niggo', 'neega', 'neegro', 'faggot', 'pakki', 'paki', 'negro', 'retard', 'retarded', 'test'];
+        if (message.author.id === '769226983078494248') {
+            message.react('💩');
+            message.reply('poop is funny');
+        };
+        const badWords =  [
+            'nigger',
+            'nigga',
+            'niggo',
+            'neega',
+            'neegro',
+            'faggot',
+            'fagot',
+            'pakki',
+            'paki',
+            'negro',
+            'retard',
+            'retarded'
+        ]
         const dontSlur = new MessageEmbed()
             .setTitle(`Don't Slur ${message.author.username}`)
             .setColor('F85056')
-            .setTimestamp()    
+            .setTimestamp()
 
         if (badWords.some(v => message.content.includes(v))) {
             message.delete()
-            message.channel.send({ embeds: [dontSlur] })
-            message.channel.send({ content: `${message.author}` })
+            message.channel.send({
+                embeds: [dontSlur]
+            })
+            message.channel.send({
+                content: `${message.author}`
+            })
         }
     },
 };
 
-// auto reactions
-//if (message.author.id === '651472179057262630') {
-//    message.react('🔥');
-//};
-//if (message.author.id === '439870891543756831') {
-//    message.react('🔥');
-//};
-//if (message.author.id === '782313476357881877') {
-//    message.react('🐦');
-//};
+//   auto reactions
+// if (message.author.id === '651472179057262630') {
+//     message.react('🔥');
+// };
+// if (message.author.id === '439870891543756831') {
+//     message.react('🔥');
+// };
+// if (message.author.id === '782313476357881877') {
+//     message.react('🐦');
+// };
 
+//   log if is a slur
 // if (badWords.some(v => message.content.includes(v))) {
 //     console.log(`Match using "${message.content}"`);
 // } else {
 //     console.log(`No match using "${message.content}"`);
 // }
+
+//   annoy
+// if (message.author.id === '815263316820754444') {
+//     message.react('💩');
+// };
