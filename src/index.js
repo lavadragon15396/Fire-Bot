d10cdadf01c06ca1c2ef74eed5187ed3e81f7aa5
Lavadragon15396 <taddufort@gmail.com>
2024-03-19 12:03:04 +0000

    index.js and basic handlers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 //* accent colour    F85056
 //* accent colour 2  F89050
 //* escape symbol    
+require("dotenv").config();
+
+const { token } = process.env;
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const handleCommands = require("./functions/handlers/handleCommands");
+
+const client = new Client({ intents: GatewayIntentBits.Guilds });
+client.commands = new Collection();
+client.commandsArray = [];
+client.colour1 = "#F85056";
+client.colour2 = "#F89050";
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+	const functionFiles = fs
+		.readdirSync(`./src/functions/${folder}`)
+		.filter((file) => file.endsWith(".js"));
+	for (const file of functionFiles)
+		require(`./functions/${folder}/${file}`)(client);
+}
+
+client.handleEvents();
+client.handleCommands();
+client.login(token);
