5de7b6be59c2f04cb5e8e8c5b12b08e8b590aca9
Lavadragon15396 <taddufort@gmail.com>
2024-03-19 12:03:09 +0000

    ready event

---
+++ src/events/client/ready.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "ready",
+    once: "true",
+    async execute(client) {
+        console.log(
+            `\n[2mStarted Bot With Token[0m: [3m[1m${process.env.token}[0m\n[2mLogged In As[0m: [3m[1m${client.user.tag} [0m`
+        );
+        client.user.setPresence({
+            activities: [{ name: `${process.env.status}`, type: `${process.env.statusType}` }],
+        });
+    }
+}
