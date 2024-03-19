2d628e5709300a8857e56d2035a5ac23bcce5efb
Lavadragon15396 <taddufort@gmail.com>
2024-03-19 12:10:26 +0000

    ping command

--- src/commands/test/Ping.js
+++ src/commands/test/Ping.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, ContextMenuCommandInteraction } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,6 +9,9 @@ module.exports = {
 			fetchReply: true
 		});
 
-		
+		const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`
+		await interaction.editReply({
+			content: newMessage
+		})
 	},
 };
